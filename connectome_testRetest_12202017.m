4391f692242457db4ff92297ede2197aabd2708d
ycollinv <yanncv76@gmail.com>
2018-01-05 14:34:59 -0500

    updated file path

--- connectome_testRetest_12202017.m
+++ connectome_testRetest_12202017.m
@@ -1,37 +1,37 @@
 clear all
 
 %% Set the template
-files_in.network = 'old/HP_project/cambridge/atlas/template_cambridge_basc_multiscale_sym_scale007.mnc';
+files_in.network = 'cdip_human_phantom/cambridge/atlas/template_cambridge_basc_multiscale_sym_scale007.mnc';
 
 %%Set the seeds
-files_in.seeds = 'old/HP_project/cambridge/seeds/ccna_seeds_cambridge7.csv';
+files_in.seeds = 'cdip_human_phantom/cambridge/seeds/ccna_seeds_cambridge7.csv';
 
 %%Set files_in
-files_in.fmri.CHUM1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUMsess1_rest1.mnc';
-files_in.fmri.CHUM2.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUMsess2_rest1.mnc';
-files_in.fmri.CHUS1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUSsess1_rest1.mnc';
-files_in.fmri.CHUS2.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUSsess2_rest1.mnc';
-files_in.fmri.CHUS3.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUSsess3_rest1.mnc';
-files_in.fmri.CINQ1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CINQsess1_rest1.mnc';
-files_in.fmri.CINQ2.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CINQsess2_rest1.mnc';
-files_in.fmri.CINQ3.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_CINQsess3_rest1.mnc';
-files_in.fmri.ISMD1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_ISMDsess1_rest1.mnc';
-files_in.fmri.ISMD2.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_ISMDsess2_rest1.mnc';
-files_in.fmri.ISMD3.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_ISMDsess3_rest1.mnc';
-files_in.fmri.IUGM1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_IUGMsess1_rest1.mnc';
-files_in.fmri.IUGM2.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_IUGMsess2_rest1.mnc';
-files_in.fmri.IUGM3.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_IUGMsess3_rest1.mnc';
-files_in.fmri.MNI1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess1_rest1.mnc';
-files_in.fmri.MNI2.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess2_rest1.mnc';
-files_in.fmri.MNI25.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess2_rest2.mnc';
-files_in.fmri.MNI3.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess3_rest1.mnc';
-%files_in.fmri.EDM1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_EDMsess1_rest1.mnc'; 
-%files_in.fmri.RRI1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_RRIsess1_rest1.mnc';
-%files_in.fmri.SASK1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_SASKsess1_rest1.mnc';
-%files_in.fmri.SUN1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_SUNsess1_rest1.mnc';
-%files_in.fmri.TWH1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_TWHsess1_rest1.mnc';
-%files_in.fmri.UBC1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_UBCsess1_rest1.mnc';
-%files_in.fmri.VIC1.session1.run1 = 'HpPreprocessFinal12202017/fmri/fmri_ssimon_VICsess1_rest1.mnc';
+files_in.fmri.CHUM1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUMsess1_rest1.mnc';
+files_in.fmri.CHUM2.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUMsess2_rest1.mnc';
+files_in.fmri.CHUS1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUSsess1_rest1.mnc';
+files_in.fmri.CHUS2.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUSsess2_rest1.mnc';
+files_in.fmri.CHUS3.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CHUSsess3_rest1.mnc';
+files_in.fmri.CINQ1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CINQsess1_rest1.mnc';
+files_in.fmri.CINQ2.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CINQsess2_rest1.mnc';
+files_in.fmri.CINQ3.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_CINQsess3_rest1.mnc';
+files_in.fmri.ISMD1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_ISMDsess1_rest1.mnc';
+files_in.fmri.ISMD2.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_ISMDsess2_rest1.mnc';
+files_in.fmri.ISMD3.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_ISMDsess3_rest1.mnc';
+files_in.fmri.IUGM1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_IUGMsess1_rest1.mnc';
+files_in.fmri.IUGM2.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_IUGMsess2_rest1.mnc';
+files_in.fmri.IUGM3.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_IUGMsess3_rest1.mnc';
+files_in.fmri.MNI1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess1_rest1.mnc';
+files_in.fmri.MNI2.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess2_rest1.mnc';
+files_in.fmri.MNI25.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess2_rest2.mnc';
+files_in.fmri.MNI3.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_MNIsess3_rest1.mnc';
+%files_in.fmri.EDM1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_EDMsess1_rest1.mnc'; 
+%files_in.fmri.RRI1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_RRIsess1_rest1.mnc';
+%files_in.fmri.SASK1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_SASKsess1_rest1.mnc';
+%files_in.fmri.SUN1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_SUNsess1_rest1.mnc';
+%files_in.fmri.TWH1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_TWHsess1_rest1.mnc';
+%files_in.fmri.UBC1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_UBCsess1_rest1.mnc';
+%files_in.fmri.VIC1.session1.run1 = 'cdip_human_phantom/HpPreprocessFinal12202017/fmri/fmri_ssimon_VICsess1_rest1.mnc';
 
 %% Options
 opt.folder_out = 'connectomeTestRetest-12202017'; % Where to store the results
